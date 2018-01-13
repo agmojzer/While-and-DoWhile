2432c65fe19f8767eac32529c6173e5d316d3191
agmojzer <agmojzer@gmail.com>
2018-01-12 20:55:27 -0500

    initial commit

---
+++ src/While.java
@@ -0,0 +1,27 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		int count = 0;
+		while(count !=6) {
+			System.out.println("Count value is " + count);
+			count++;
+		}
+			System.out.println("******");
+			
+		for(int i=0; i<6; i++) {
+			System.out.println("Count value is " + i);
+		}
+		count = 1;
+		while(true) {
+			if(count == 5) {
+				break;}
+		
+		
+		
+			
+		}
+		}
+	//the above for and while statements print the same output.
+
+}
